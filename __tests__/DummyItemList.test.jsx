11d2abe22cc210a7791f42428f023cdb62a5300f
timotius <timotis@ruangalternative.com>
2022-08-01 20:01:47 +0700

    add react-testing-library

---
+++ __tests__/DummyItemList.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import DummyItemList from "common/components/DummyItemList/DummyItemList";
+
+describe("DummyItemList", () => {
+  it("renders a heading", () => {
+    render(<DummyItemList code={"saya"} name={"wdhaw"} />);
+
+    const heading = screen.getByText("saya - wdhaw");
+    screen.debug();
+
+    expect(heading).toBeInTheDocument();
+  });
+});
